9017065492e27614cf4fec81443d01f77b267a8b
hasan-py <hasan29351@gmail.com>
2022-07-14 17:06:36 +0600

    Mongodb Could Url Added

--- server/app.js
+++ server/app.js
@@ -1,59 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // Import Router
-const authRouter = require('./routes/auth');
-const categoryRouter = require('./routes/categories');
-const productRouter = require('./routes/products');
-const brainTreeRouter = require('./routes/braintree');
-const orderRouter = require('./routes/orders');
-const usersRouter = require('./routes/users');
-const customizeRouter = require('./routes/customize');
+const authRouter = require("./routes/auth");
+const categoryRouter = require("./routes/categories");
+const productRouter = require("./routes/products");
+const brainTreeRouter = require("./routes/braintree");
+const orderRouter = require("./routes/orders");
+const usersRouter = require("./routes/users");
+const customizeRouter = require("./routes/customize");
 // Import Auth middleware for check user login or not~
-const { loginCheck } = require('./middleware/auth');
-const CreateAllFolder = require('./config/uploadFolderCreateScript');
+const { loginCheck } = require("./middleware/auth");
+const CreateAllFolder = require("./config/uploadFolderCreateScript");
 
 /* Create All Uploads Folder if not exists | For Uploading Images */
 CreateAllFolder();
 
+// This will be the cloud mongodb URL
+/* N.B: Please Add this string to the .env file for production */
+const uri =
+  "mongodb+srv://seyramalifo:Applemania96@cluster0.lmwlz9v.mongodb.net/autoparts_ecommerce";
+
 // Database Connection
 mongoose
-  .connect(process.env.DATABASE_URL, {
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
   .then(() =>
     console.log(
-      '==============Mongodb Database Connected Successfully=============='
+      "==============Mongodb Database Connected Successfully=============="
     )
   )
   .catch((err) => console.log(err));
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Routes
-app.use('/api', authRouter);
-app.use('/api/user', usersRouter);
-app.use('/api/category', categoryRouter);
-app.use('/api/product', productRouter);
-app.use('/api', brainTreeRouter);
-app.use('/api/order', orderRouter);
-app.use('/api/customize', customizeRouter);
+app.use("/api", authRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
+app.use("/api", brainTreeRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/customize", customizeRouter);
 
 // Run Server
 const PORT = process.env.PORT || 8900;
 app.listen(PORT, () => {
-  console.log('Server is running on ', PORT);
+  console.log("Server is running on ", PORT);
 });
